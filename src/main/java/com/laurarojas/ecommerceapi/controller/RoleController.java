56fe4623cfd501736e59e4568735c286c390319a
laura260489 <lauradiezmil@hotmail.com>
2025-08-05 00:15:32 -0500

    Enable cors

--- src/main/java/com/laurarojas/ecommerceapi/controller/RoleController.java
+++ src/main/java/com/laurarojas/ecommerceapi/controller/RoleController.java
@@ -2,10 +2,12 @@ package com.laurarojas.ecommerceapi.controller;
 
 import com.laurarojas.ecommerceapi.service.RoleService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api")
 public class RoleController {
--- src/main/java/com/laurarojas/ecommerceapi/controller/UserController.java
+++ src/main/java/com/laurarojas/ecommerceapi/controller/UserController.java
@@ -4,6 +4,7 @@ import com.laurarojas.ecommerceapi.dtos.ResponseMessageDTO;
 import com.laurarojas.ecommerceapi.dtos.UserUpdateDTO;
 import com.laurarojas.ecommerceapi.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api")
 public class UserController {
